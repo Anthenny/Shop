ee219abfe8e90cd28a043bbca8050143e18fc63f
Anthenny de Hoon <Anthenny1997@hotmail.com>
2021-06-07 20:02:29 +0200

    Verbinding met database

--- Ecommerce/app.js
+++ Ecommerce/app.js
@@ -1,12 +1,12 @@
 // Modules
 
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-dotenv.config({ path: "./config.env" });
+// dotenv.config({ path: "./config.env" });
 
 const shopRoutes = require("./routes/shop");
 const port = process.env.PORT;
@@ -18,12 +18,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(shopRoutes);
 
-// app punt listen Routes css in public file stoppen. ejs bestanden maken.shopController EJS?
-
-// css filve verplaatsen in public anders error
-// producten.html veranderen in oorbellen.html anders error.
-// Fotos aanpassen in scss index.html zodat mobiel werkt
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
---
+++ Ecommerce/server.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE;
+const port = process.env.PORT;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection succesful");
+    app.listen(port, () => {
+      console.log(`App running on ${port}`);
+    });
+  })
+  .catch((e) => {
+    // Hier doorverwijzing geven naar een pagina die zegt dat onze database down is.
+    console.log(e.message);
+  });
