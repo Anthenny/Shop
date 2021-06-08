1dca3779a1e17e72e1511d96e72e2827e184c90c
Anthenny de Hoon <Anthenny1997@hotmail.com>
2021-06-08 23:46:51 +0200

    bruh

--- Ecommerce/routes/admin.js
+++ Ecommerce/routes/admin.js
@@ -1,14 +1,9 @@
 const express = require("express");
-
 const adminController = require("../controllers/admin");
-
 const router = express.Router();
 
-// alle GET methods
 router.get("/Add-product", adminController.getAddEditProduct);
 router.get("/admin", adminController.getProducts);
-
-// alle POST methods
 router.post("/Add-product", adminController.postAddproduct);
 router.post("/delete-product", adminController.postDeleteProduct);
 
--- Ecommerce/routes/auth.js
+++ Ecommerce/routes/auth.js
@@ -10,16 +10,18 @@ router.get("/logout", authController.getLogout);
 router.post(
   "/signup",
   [
-    check("email").isEmail().withMessage("Vul aub een email in."),
-    body("password", "Vul aub een wachtwoord in met minimaal 5 tekens.").isLength({ min: 5 }),
-    body("confirmPassword").custom((value, { req }) => {
-      if (value !== req.body.password) throw new Error("Wachtwoord moet met elkaar matchen!");
-      return true;
-    }),
+    check("email").isEmail().withMessage("Vul aub een geldig email adres in..").normalizeEmail(),
+    body("password", "Vul aub een wachtwoord in met minimaal 5 tekens.").isLength({ min: 5 }).trim(),
+    body("confirmPassword")
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) throw new Error("Wachtwoord moet met elkaar matchen!");
+        return true;
+      }),
   ],
   isAuth.isAuth,
   authController.postSignUp
 );
-router.post("/login", isAuth.isAuth, authController.postLogIn);
+router.post("/login", [body("email").isEmail().withMessage("Vul aub een geldig email adres in.").normalizeEmail(), body("password").trim()], isAuth.isAuth, authController.postLogIn);
 
 module.exports = router;
