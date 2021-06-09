18668c032b4f5feecd1b56d1f035e75f32016e06
Anthenny de Hoon <Anthenny1997@hotmail.com>
2021-06-09 22:24:04 +0200

    extra producten toegevoegd met routes en dropdown menu

--- Ecommerce/controllers/shopController.js
+++ Ecommerce/controllers/shopController.js
@@ -32,3 +32,38 @@ exports.getProfiel = (req, res) => {
     path: "/profiel",
   });
 };
+
+exports.getArmbanden = (req, res) => {
+  res.status(200).render("shop/armbanden", {
+    pageTitle: "Armbanden",
+    path: "/armbanden",
+  });
+};
+
+exports.getTassen = (req, res) => {
+  res.status(200).render("shop/tassen", {
+    pageTitle: "Tassen",
+    path: "/tassen",
+  });
+};
+
+exports.getWaxmelts = (req, res) => {
+  res.status(200).render("shop/waxmelts", {
+    pageTitle: "Waxmelts",
+    path: "/waxmelts",
+  });
+};
+
+exports.getBabyAccessoires = (req, res) => {
+  res.status(200).render("shop/babyaccessoires", {
+    pageTitle: "Baby Accessoires",
+    path: "/babyaccessoires",
+  });
+};
+
+exports.getProductSpecificatie = (req, res) => {
+  res.status(200).render("shop/productSpecs", {
+    pageTitle: "Product Specificatie",
+    path: "/productSpecificatie",
+  });
+};
--- Ecommerce/routes/shop.js
+++ Ecommerce/routes/shop.js
@@ -4,9 +4,14 @@ const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 router.get("/", shopController.getHome);
-router.get("/bestellingen", isAuth.isNotAuth, shopController.getBestellingen);
 router.get("/winkelmand", isAuth.isNotAuth, shopController.getWinkeland);
 router.get("/oorbellen", shopController.getOorbellen);
+router.get("/armbanden", shopController.getArmbanden);
+router.get("/tassen", shopController.getTassen);
+router.get("/waxmelts", shopController.getWaxmelts);
+router.get("/babyaccessoires", shopController.getBabyAccessoires);
+router.get("/productSpecificatie", shopController.getProductSpecificatie);
 router.get("/profiel", isAuth.isNotAuth, shopController.getProfiel);
+router.get("/bestellingen", isAuth.isNotAuth, shopController.getBestellingen);
 
 module.exports = router;
